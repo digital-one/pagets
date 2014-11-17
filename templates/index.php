ebcdc52ad2f455fbdddc0f0cb08824b8f537712f
digital-one <neil@digital-one.co.uk>
2014-11-17 20:59:29 +0000

    template HTML

---
+++ templates/index.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+        <link rel="stylesheet" href="css/layout.css" />
+        <link rel="stylesheet" href="js/slick/slick.css" />
+        <script src="js/modernizr.js"></script>
+<!--[if (gte IE 6)&(lte IE 8)]>
+<script src="js/selectivizr-min.js"></script>
+<![endif]-->
+        <!--[if lte IE 9]>
+          <script src="js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+<header id="header">
+<div class="container">
+<h1 id="home-link"><img src="images/pagets.svg" alt="Paget's Association" /></h1>
+<!--secondary nav-->
+<nav id="secondary-nav" role="navigation"><ul><li><a href="#">About Us</a></li><li><a href="#">Membership / Login</a></li><li><a href="#">Remember Someone</a></li><li><a href="#">Shop</a></li><li><a href="#">Contact Us</a></li></ul></nav>
+<form id="search" method="post" action=""><input type="text" placeholder="Search" /><button type="submit">Search</button></form>
+<!--/secondary nav-->
+<div id="ribbon"><div><span>General Info <a href="tel:01617994646">0161 799 4646</a></span><span>Nurse Helpline <a href="tel:07713568197">07713 568197</a></span></div></div>
+</div>
+</header>
+<!--/header-->
+<!--main nav-->
+<nav id="nav" role="navigation">
+  <div class="container">
+<ul><li class="current-menu-item"><a href="">Home</a></li><li><a href="">Information &amp; Support</a></li><li><a href="">Research</a></li><li><a href="">Professionals</a></li><li><a href="">News &amp; Events</a></li><li><a href="">Get Involved</a></li></ul>
+<a href="" class="donate">Donate</a>
+</div>
+</nav>
+<!--/main nav-->
+
+<!--breadcrumb-->
+<div id="breadcrumb"><div class="container"><a href="">Home</a> > <a href="">Information &amp; Support</a> > Local Support</div></div>
+<!--/breadcrumb-->
+
+<!--content-->
+<div id="content" class="container">
+  <!--slider-->
+<section id="slider">
+<!--slide-->
+<div class="slide">
+  <aside><h3>The Paget’s Association is the only UK charity to focus solely on Paget’s disease of bone</h3><p>It acts as a resource for those with the condition, the public and health professionals, offering high quality information and support.</p><a href="" class="button">Find out more</a></aside>
+  <div class="image" style="background-image:url('images/slide-img.jpg');"></div>
+</div>
+<!--/slide-->
+<!--slide-->
+<div class="slide">
+  <aside><h3>The Paget’s Association is the only UK charity to focus solely on Paget’s disease of bone</h3><p>It acts as a resource for those with the condition, the public and health professionals, offering high quality information and support.</p><a href="" class="button">Find out more</a></aside>
+  <div class="image" style="background-image:url('images/slide-img.jpg');"></div>
+</div>
+<!--/slide-->
+
+</section>
+<!--/slider-->
+
+  <!--signposts-->
+  <section id="signposts" class="arrows">
+<div>
+<h3>Information</h3>
+<ul>
+  <li><a href="#">Paget’s Disease</a></li>
+<li><a href="#">Treatment</a></li>
+<li><a href="#">Centres of Excellence</a></li>
+<li><a href="#">Paget’s Newsletter</a></li>
+</ul>
+</div>
+<div>
+<h3>Support</h3>
+<ul>
+  <li><a href="#">Patients &amp; Carers</a></li>
+<li><a href="#">Nurse Helpline</a></li>
+<li><a href="#">Professionals</a></li>
+</ul>
+</div>
+<div>
+<h3>Get Involved</h3>
+<ul>
+  <li><a href="#">Donate</a></li>
+<li><a href="#">Take part in an event</a></li>
+<li><a href="#">Help raise funds</a></li>
+</ul>
+</div>
+  </section>
+  <!--/signposts-->
+  <!--news and events -->
+<section id="news-events">
+  <h3>News &amp; Events</h3>
+<div id="carousel">
+<div class="article"><a href="#"><figure><img src="images/carousel-img-1.jpg" /></figure><p>Article title ipsum dolor sit amet consectetuer adipiscing elit sed diam nonummy nibh euismod</p></a></div>
+<div class="article"><a href="#"><figure><img src="images/carousel-img-2.jpg" /></figure><p>Article title ipsum dolor sit amet consectetuer adipiscing elit sed diam nonummy nibh euismod</p></a></div>
+<div class="article"><a href="#"><figure><img src="images/carousel-img-3.jpg" /></figure><p>Article title ipsum dolor sit amet consectetuer adipiscing elit sed diam nonummy nibh euismod</p></a></div>
+<div class="article"><a href="#"><figure><img src="images/carousel-img-1.jpg" /></figure><p>Article title ipsum dolor sit amet consectetuer adipiscing elit sed diam nonummy nibh euismod</p></a></div>
+<div class="article"><a href="#"><figure><img src="images/carousel-img-2.jpg" /></figure><p>Article title ipsum dolor sit amet consectetuer adipiscing elit sed diam nonummy nibh euismod</p></a></div>
+<div class="article"><a href="#"><figure><img src="images/carousel-img-3.jpg" /></figure><p>Article title ipsum dolor sit amet consectetuer adipiscing elit sed diam nonummy nibh euismod</p></a></div>
+</div>
+<footer><a href=""><span>View All</span></a></footer>
+</section>
+<!--/news and events -->
+<!--newsletter-->
+<a href="" id="newsletter">
+<h4><strong>Free</strong> paget’s newsletter</h4>
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+</a>
+<!--/newsletter-->
+<!--social-->
+<section id="social-links">
+<nav>
+  <h5>Follow us</h5>
+<ul>
+  <li><a href="">Google Plus</a></li>
+    <li><a href="" class="facebook">Facebook</a></li>
+      <li><a href="" class="twitter">Twitter</a></li>
+        <li><a href="" class="pinterest">Pinterest</a></li>
+          <li><a href="" class="linkedin">Linkedin</a></li>
+        </ul>
+      </nav>
+</section>
+<!--sidebar-->
+<aside id="sidebar">
+</aside>
+<!--/sidebar-->
+<!--main-->
+<div id="main" role="main">
+
+</div>
+<!--/main-->
+</div>
+<!--/content-->
+<!--footer-->
+<footer id="footer">
+<div class="container">
+  <!--left column-->
+<div class="column left">
+<nav id="footer-nav"><ul><li><a href="">Site Map</a></li><li><a href="">Privacy Policy</a></li><li><a href="">Terms of Use</a></li></ul></nav>
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip.</p>
+<div class="copyright fit-to-base">
+<small>&copy; Paget's Association Charity No: 266071</small>
+<address>Suite 5, Moorfield House, Moorside Road, Swinton, Manchester M27 0EW</address>
+</div>
+</div>
+<!--/left column-->
+<!--right column-->
+<div class="column right">
+  <div id="logos" class="row">
+<a href=""><span class="logo"><img src="images/the-best-of-salford.png" alt="the best of salford" /></span></a>
+<a href=""><span class="logo"><img src="images/health-unlocked.jpg" alt="Health Unlocked" /></span><span>Connect with others who understand Paget’s disease - <strong>Join today!</strong></span></a>
+</div>
+<div class="row fit-to-base">
+<a href="" class="ama">AMA</a>
+</div>
+  </div>
+<!--/right column-->
+</div>
+</footer>
+<!--/footer-->
+<!--scripts-->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<!-- Load jQuery from a local copy if loading from Google fails -->
+<script>window.jQuery || document.write('<script type="text/javascript" src="js/jquery-1.10.1.min.js"><\/script>')</script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+<!-- Load jQuery UI from a local copy if loading from Google fails -->
+<script>window.jQuery || document.write('<script type="text/javascript" src="js/jquery-ui.min.js"><\/script>')</script>
+<script src="//maps.google.com/maps/api/js?sensor=true"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/slick/slick.min.js"></script>
+<script src="js/scripts.js"></script>
+<!--/scripts-->
+</body>
+</html>
---
+++ templates/js/scripts.js
@@ -0,0 +1,42 @@
+$(function(){
+	$('#slider').slick({
+		dots: true,
+		autoplay: true,
+  		autoplaySpeed: 2000,
+  		arrows: false
+	});
+
+	$('#carousel').slick({
+  		dots: false,
+  		arrows: true,
+  		infinite: true,
+  		speed: 300,
+  		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		responsive: [
+    	{
+      		breakpoint: 1024,
+      		settings: {
+        	slidesToShow: 3,
+        	slidesToScroll: 1,
+        	infinite: true,
+        	dots: true
+      	}
+    },
+    	{
+      		breakpoint: 600,
+      		settings: {
+        	slidesToShow: 2,
+        	slidesToScroll: 1
+      	}
+    },
+    	{
+      	breakpoint: 480,
+      	settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+});
